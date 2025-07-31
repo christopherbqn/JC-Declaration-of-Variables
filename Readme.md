161d43c558ccf96a33aa153536c6319ef5bedb2b
christopher.bayquen <christopher.bayquen@gcash.com>
2025-07-31 16:15:12 +0800

    Lab : Declaration of Variables Submission

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Lab : Java Declaration of Variables
+
+Submitted by: Bayquen, Christopher Gilbert A. | July 31, 2025
+
+**Output**
+![Sample Input/Output Screenshot](/images/output.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,54 @@ package org.example;
 
 public class Main {
 
+    // instance variables
+    int instanceBinary = 0b101101; // 45 in decimal
+    int instanceOctal = 072;       // 58 in decimal
+    double instanceDouble = 1_234.95;
+
+    // local variables
+    public static void localVariables(){
+
+        int localDecimal = 100;
+        int localHex = 0xFF; // 255 in Hex
+        int localBinary = 0b1101; // 100 in Binary
+        int localOctal = 014; // 12 in Octal
+        long localLong = 10000000L;
+        String localString = "Every step I take gets me closer to my goal.";
+
+        System.out.println("Local Variables:");
+        System.out.println("  localDecimal = " + localDecimal);
+        System.out.println("  localHex = " + localHex);
+        System.out.println("  localOctal = " + localOctal);
+        System.out.println("  localLong = " + localLong);
+        System.out.println("  localString = " + localString);
+        System.out.println();
+    }
+
+    // static variables
+    static int staticHex = 0x1A2B; // 6699 in decimal
+    static byte staticByte = 1;
+    static String staticString = "Progress, not perfection.";
 
     public static void main(String[] args) {
 
+        // call local variables
+        localVariables();
 
+        // instance variables print
+        Main obj = new Main();
+
+        System.out.println("Instance Variables:");
+        System.out.println("  instanceBinary = " + obj.instanceBinary);
+        System.out.println("  instanceOctal = " + obj.instanceOctal);
+        System.out.println("  instanceDouble = " + obj.instanceDouble);
+        System.out.println();
+
+        // static variables print
+        System.out.println("Static Variables:");
+        System.out.println("  staticHex = " + staticHex);
+        System.out.println("  staticByte = " + staticByte);
+        System.out.println("  staticString = " + staticString);
     }
+
 }
